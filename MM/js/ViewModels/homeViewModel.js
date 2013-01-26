21ed77e8c31e1f3429e9feea64afc3d37abd7072
Tom Krueger <tom@marmotmobile.com>
2013-01-26 17:18:51 -0600

    unindented code

--- MM/js/ViewModels/homeViewModel.js
+++ MM/js/ViewModels/homeViewModel.js
@@ -1,59 +1,55 @@
+'use strict';
 
 var app = app || {};
 
-//(function() {
-	'use strict';
 
-    // our main view model
-	app.HomeViewModel = function() {
-		var nearByCommunities = ko.observableArray(),
-            nearByOffers = ko.observableArray();
-        
-        // Behaviours.
-        var load = function(zipCode) {
-            
-            app.Services.Community.getNearByCommunities(
-                zipCode,
-                function(communitiesDto) {
-                    nearByCommunities.removeAll();
-            
-                    communitiesDto.forEach(function(communityDto) {
-                       
-                        var model = new app.Models.Community();
-                        model.name = communityDto.name;
-                        model.imageUrl = communityDto.imageUrl;
-                        
-                        nearByCommunities.push(model); 
-                        
-                    });
-                                           
-                              
-                });
-            
-            app.Services.Offer.getNearByOffers(
-                zipCode,
-                function(offersDto) {
-                    nearByOffers.removeAll();
-            
-                    offersDto.forEach(function(offerDto) {
-                                        
-                        var model = new app.Models.Offer();
-                        model.name = offerDto.name;
-                        model.imageUrl = offerDto.imageUrl;
-                        model.distance = offerDto.dist;
-                        
-                        nearByOffers.push(model);  
-                    });
-                    
-                    $("#offersSection ul").listview("refresh");
-                });
-        };       
-        
-        return {
-            communities: nearByCommunities,
-            offers: nearByOffers,
-            load: load  
-        };
-    };
+app.HomeViewModel = function() {
+    var nearByCommunities = ko.observableArray(),
+        nearByOffers = ko.observableArray();
     
-//}());
+    // Behaviours.
+    var load = function(zipCode) {
+        
+        app.Services.Community.getNearByCommunities(
+            zipCode,
+            function(communitiesDto) {
+                nearByCommunities.removeAll();
+        
+                communitiesDto.forEach(function(communityDto) {
+                   
+                    var model = new app.Models.Community();
+                    model.name = communityDto.name;
+                    model.imageUrl = communityDto.imageUrl;
+                    
+                    nearByCommunities.push(model); 
+                    
+                });
+                                       
+                          
+            });
+        
+        app.Services.Offer.getNearByOffers(
+            zipCode,
+            function(offersDto) {
+                nearByOffers.removeAll();
+        
+                offersDto.forEach(function(offerDto) {
+                                    
+                    var model = new app.Models.Offer();
+                    model.name = offerDto.name;
+                    model.imageUrl = offerDto.imageUrl;
+                    model.distance = offerDto.dist;
+                    
+                    nearByOffers.push(model);  
+                });
+                
+                $("#offersSection ul").listview("refresh");
+            });
+    };       
+    
+    return {
+        communities: nearByCommunities,
+        offers: nearByOffers,
+        load: load  
+    };    
+};
