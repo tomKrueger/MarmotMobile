d1c7336de2478a5023458be86fcc857fab99db34
tom@marmotmobile.com <tom@marmotmobile.com>
2013-03-01 07:39:12 -0600

    Future code for handling device specific settings

--- MM/js/appBase.js
+++ MM/js/appBase.js
@@ -8,6 +8,20 @@ var app = app || {};
 	//	var _gaq=[['_setAccount','UA-31081062-1'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'));
 	//}
 
+    /*
+    // Handle device specific settings.
+    var deviceAgent = navigator.userAgent.toLowerCase();
+    var agentID = deviceAgent.match(/(iphone|ipod|ipad|android)/);
+    if(agentID.indexOf("iphone") >= 0) {
+        alert("iphone");
+    } else if(agentID.indexOf("ipod") >= 0 ) {
+        alert("ipod");
+    } else if(agentID.indexOf("ipad") >= 0 ) {
+        alert("ipad");
+    } else if(agentID.indexOf("android") >= 0 ) {
+        alert("android");
+    }
+    */
     
 })( window );
 
